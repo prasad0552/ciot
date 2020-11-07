73f2b551219ef4449af002fa6455df8198c8ac73
VaraPrasad <prasad0552@gmail.com>
2020-11-07 23:02:33 +0530

    Changes with reverted files

--- modules/SwVtTools/constants.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by JetBrains PhpStorm.
- * User: Stefan Warnat <support@stefanwarnat.de>
- * Date: 24.07.14 14:59
- * You must not use this file without permission.
- */
-
-define("CACHE_DIR", dirname(__FILE__).DIRECTORY_SEPARATOR."cache/");
